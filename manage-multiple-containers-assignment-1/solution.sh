7becafe8d4eb9c05f737c573b6acf4fe3b216f50
Nate Stott <nate.stott@pm.me>
2025-05-21 19:40:39 +0000

    add tests

--- manage-multiple-containers-assignment-1/solution.sh
+++ manage-multiple-containers-assignment-1/solution.sh
@@ -4,6 +4,8 @@ docker container run -p 80:80 --name nginx -d nginx
 docker container run -p 8080:80 --name httpd -d httpd
 docker container run -p 3306:3306 --name mysql -d --env MYSQL_RANDOM_ROOT_PASSWORD=yes mysql
 docker container ls -a
+curl localhost
+curl localhost:8080
 docker container logs mysql 2>&1 | grep -i 'GENERATED'
 docker container rm -f mysql httpd nginx
 docker container ls -a
